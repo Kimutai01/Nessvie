4900c9eeadab546ff38b356a9f4232b6d0b84a88
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-06-19 09:18:19 +0300

    Added home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ profile }) => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="hom">
+      <h1 className="text-2xl  font-bold text-center ">
+        {" "}
+        Welcome to Nessvie where we bring your dream job next to you.
+      </h1>
 
-export default Home
+      {profile ? (
+        <>
+          <button>
+            <a href="/profile">View Profile</a>
+          </button>
+          <button>view jobs</button>
+        </>
+      ) : (
+        <button className="selector text-white p-2 ml-10 rounded-xl text-center">
+          <Link to="/profile">Create profile</Link>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Home;
